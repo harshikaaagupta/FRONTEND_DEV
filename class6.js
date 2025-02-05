9d019689833d94988a088a61a0f7485649934b18
Harshika Gupta <harshikagupta098@gmail.com>
2025-02-05 09:00:12 +0530

    update

---
+++ class6.js
@@ -0,0 +1,22 @@
+//let a="abc";
+//let b="def";
+//let c=a+b;
+//console.log(c);
+
+//Print sum of two numbers.
+//let a=0;
+//let b=1;
+//let c=a+b;
+//console.log(c);
+
+let a=0;
+let b=1;
+let num;
+console.log("Fibonacci series:");
+for(let i=1;i<=10;i++){
+    console.log(a);
+    num=a+b;
+    a=b;
+    b=num;
+
+}
---
+++ class9.js
@@ -0,0 +1,17 @@
+/*
+console.log("Hello World");
+console.warn("This is a warning");
+console.error("This is an error");
+console.log("This is a write");
+console.info("This is your information");*/
+
+// Operators
+// Arithmetic Operator:+,-,/,*,%
+// Logical Operator:&&,OR,NOT
+// Assignment Operator:>=,<,>,=<
+
+// var a;
+// a=null;
+// console.log(a);
+
+// FUNCTIONS
